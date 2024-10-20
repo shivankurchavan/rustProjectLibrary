b3502262255c9aea38fc534d95e52c4ca79afae1
shivankurchavan <shivankurchavan@gmail.com>
2024-10-21 03:06:42 +0530

    city input updated

--- weatherapp/src/main.rs
+++ weatherapp/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::io;
 use dotenv::dotenv;
 
 mod api;
@@ -6,7 +7,12 @@ mod api;
 #[tokio::main]
 async fn main(){
     dotenv().ok();
-    let city = "Mumbai";
+    let mut input = String::new();
+    println!("Please enter City Name:");
+    io::stdin().read_line(&mut input).expect("Failed");
+
+
+    let city = input.trim();
     let api_key = env::var("API_KEY").expect("API_KEY not set in .env file");
 
     match api::get_forcast(city, &api_key).await {
