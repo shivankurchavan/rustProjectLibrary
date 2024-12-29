08e01f0cc48a45cb321ad0ec01e2f667db3bcfeb
shivankurchavan <shivankurchavan@gmail.com>
2024-12-30 05:25:20 +0530

    rustgoogle

--- rustGoogle/src/main.rs
+++ rustGoogle/src/main.rs
@@ -10,7 +10,21 @@ fn variables(a:i32){
     return;
 }
 
+fn takes_u32(x: u32) {
+    println!("u32: {x}");
+}
+
+fn takes_i8(y: i8) {
+    println!("i8: {y}");
+}
+
 fn main() {
     variables(10);
     println!("result {}", arithmatic(10,20));
+
+    let x = 10;
+    let y = 20;
+
+    takes_u32(x);
+    takes_i8(y);
 }
